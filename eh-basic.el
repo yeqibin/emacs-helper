e438ac53d651cf14230095245e204bfc491d43ae
Feng Shu <tumashu@163.com>
2016-02-15 10:34:13 +0800

    update

--- eh-basic.el
+++ eh-basic.el
@@ -224,7 +224,7 @@
         pyim-page-length 5)
   :bind
   (("C-<SPC>" . toggle-input-method)
-   ("C-t" . pyim-convert-pinyin-at-point)
+   ("C-i" . pyim-convert-pinyin-at-point)
    ("C-;" . pyim-toggle-input-ascii)
    ("C-:" . pyim-delete-word-from-personal-buffer)))
 
