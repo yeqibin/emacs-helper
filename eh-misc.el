d236070ad1f6268446795988ff50868ebfad6054
Feng Shu <tumashu@163.com>
2016-02-25 20:37:36 +0800

    update

--- eh-misc.el
+++ eh-misc.el
@@ -209,8 +209,8 @@
 (use-package switch-window
   :bind (("C-x o" . switch-window)
          ("C-x 1" . switch-window-then-maximize)
-         ("C-x 2" . switch-window-then-split-vertically)
-         ("C-x 3" . switch-window-then-split-horizontally)
+         ("C-x 2" . switch-window-then-split-below)
+         ("C-x 3" . switch-window-then-split-right)
          ("C-x 0" . switch-window-then-delete))
   :config
   (setq switch-window-increase 8)
